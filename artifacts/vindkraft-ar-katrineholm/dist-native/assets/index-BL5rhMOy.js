4af81056a964febf557677227cf83a765a8a4862
poyansandnell <poyan.sandnell@catchme.se>
2026-08-05 08:13:27 +0000

    Set VITE_API_BASE_URL=https://katrineholm.replit.app for native builds
    - .env.native: ersätter riker.replit.dev-tunneln med katrineholm.replit.app
    - VITE_PUBLIC_APP_URL: satt till samma stabila produktions-URL
    - dist-native/ ombyggd med ny URL inbakad (pnpm native:sync)
    - Inga .replit.dev-adresser kvar i native-konfigurationen

--- artifacts/vindkraft-ar-katrineholm/dist-native/assets/index-BL5rhMOy.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DEZtKQre.js","assets/index-BCYBEhrD.js","assets/index-Brat--Ag.css"])))=>i.map(i=>d[i]);
-import{r,_ as a}from"./index-BCYBEhrD.js";const t=r("CameraPreview",{web:()=>a(()=>import("./web-DEZtKQre.js"),__vite__mapDeps([0,1,2])).then(e=>new e.CameraPreviewWeb)});export{t as CameraPreview};
