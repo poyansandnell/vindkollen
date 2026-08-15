8137453b094789dfd9fa3c91f21341886805831e
poyansandnell <poyan.sandnell@catchme.se>
2026-08-15 15:32:41 +0000

    Bump iOS version to 1.1 (build 2) for TestFlight — GPS fix + crash guards

--- artifacts/vindkraft-ar-katrineholm/dist-native/assets/index-D-kkSzZ8.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BwvuLhDI.js","assets/index-CMl8ayxx.js","assets/index-H4cXKivg.css"])))=>i.map(i=>d[i]);
-import{r,_ as a}from"./index-CMl8ayxx.js";const t=r("CameraPreview",{web:()=>a(()=>import("./web-BwvuLhDI.js"),__vite__mapDeps([0,1,2])).then(e=>new e.CameraPreviewWeb)});export{t as CameraPreview};
