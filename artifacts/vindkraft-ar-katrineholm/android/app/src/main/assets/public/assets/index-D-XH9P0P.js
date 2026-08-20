3b1b4edc6ffe2a033ac43ea630a8b58cc45ac5dc
poyansandnell <50151399-poyansandnell@users.noreply.replit.com>
2026-08-20 09:56:57 +0000

    Git commit prior to merge

--- artifacts/vindkraft-ar-katrineholm/android/app/src/main/assets/public/assets/index-D-XH9P0P.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-NvkwjGS_.js","assets/index-BXCzFm7x.js","assets/index-H4cXKivg.css"])))=>i.map(i=>d[i]);
-import{r,_ as a}from"./index-BXCzFm7x.js";const t=r("CameraPreview",{web:()=>a(()=>import("./web-NvkwjGS_.js"),__vite__mapDeps([0,1,2])).then(e=>new e.CameraPreviewWeb)});export{t as CameraPreview};
