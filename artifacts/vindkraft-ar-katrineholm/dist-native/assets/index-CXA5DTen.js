093b164c74a3993846a235e3bf15bb40e5534ba5
poyansandnell <poyan.sandnell@catchme.se>
2026-08-24 06:31:08 +0000

    Make real turbine scale the AR default

---
+++ artifacts/vindkraft-ar-katrineholm/dist-native/assets/index-CXA5DTen.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-i1OfYJ-N.js","assets/index-DW3D9mGV.js","assets/index-jyi5aaHF.css"])))=>i.map(i=>d[i]);
+import{r,_ as a}from"./index-DW3D9mGV.js";const t=r("CameraPreview",{web:()=>a(()=>import("./web-i1OfYJ-N.js"),__vite__mapDeps([0,1,2])).then(e=>new e.CameraPreviewWeb)});export{t as CameraPreview};
